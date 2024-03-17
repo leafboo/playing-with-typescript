1e413875a684fb0b9fba2495ca6601282801384e
Jan romel <janjancaranguian@gmail.com>
2024-03-17 20:12:31 +0800

    Play with danbooru api

--- src/index.ts
+++ src/index.ts
@@ -5,5 +5,20 @@ console.log('hello')
 if (descriptionElement && pressMeElement) { 
   pressMeElement?.addEventListener('click', () => {
     descriptionElement.textContent = "You pressed the button"
+    fetchData()
   })
 }
+
+
+// api calls
+async function fetchData() {
+  try {
+    const response = await fetch('https://danbooru.donmai.us/artists/67359.json')
+
+    const data: any = await response.json()
+    console.log(data)
+  } catch(err) {
+    console.error(`Error fetching data: ${err}`)
+  }
+}
+
